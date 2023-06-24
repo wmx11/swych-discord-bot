9c8be286cf2322bd3076e2772ef254f7d3956d35
Vainius <vmykolaitis1@gmail.com>
2023-06-24 15:19:32 +0300

    Trained data

--- src/qna/content/contract-address.ts
+++ src/qna/content/contract-address.ts
@@ -12,9 +12,9 @@ export default {
   reactions: [],
   answers: [
     `
-  **Swych Contract Address:**
-  0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3
+**Swych Contract Address:**
+0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3
   `,
-    `The Swych contract address is 0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3`,
+    `The Swych contract address is **0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3**`,
   ],
 } as Qna;
