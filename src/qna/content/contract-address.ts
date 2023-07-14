63c838d4df7e976caf5d2610633eb0b4d680c5cb
Vainius <vmykolaitis1@gmail.com>
2023-07-14 11:08:27 +0300

    Tweaked questions

--- src/qna/content/contract-address.ts
+++ src/qna/content/contract-address.ts
@@ -15,6 +15,5 @@ export default {
 **Swych Contract Address:**
 0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3
   `,
-    `The Swych contract address is **0x9334e37faD7c41Cd6C9565Bff3A97CE31CEE52a3**`,
   ],
 } as Qna;
--- src/qna/content/i-cant-sell.ts
+++ src/qna/content/i-cant-sell.ts
@@ -6,12 +6,15 @@ export default {
     "why can't I sell",
     'how do I sell',
     'it gives me an error',
+    'gives me an error',
     "can't swap swych",
     "can't swap my tokens",
     'how to sell',
     "why I can't sell",
-    'The input token cannot be transferred',
+    "can't sell",
+    'the input token cannot be transferred',
     '>atb',
+    "can't swap on swych",
   ],
   enabled: true,
   reactions: [],
@@ -27,6 +30,5 @@ export default {
   
   • Find your ATB limits here:
   https://app.swych.finance/staking`,
-    `Please make sure you are following your ATB limits by selling up to 20% of your tokens every 10 days. Also, make sure you set the slippage at 12% and you are swapping to BNB tokens.`,
   ],
 } as Qna;
